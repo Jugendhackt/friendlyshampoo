fd52d8f1d471092716965a96acd6f91c11a58e4f
TestTest784 <56189374+TestTest784@users.noreply.github.com>
2022-06-19 09:41:42 +0200

    code verbessern und funktionen schreiben :D

--- scraper.py
+++ scraper.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import requests
 from bs4 import BeautifulSoup
 
@@ -40,39 +39,3 @@ for artikelnummer in alle_produkte.keys():
 print(alle_produkte)
 
 
-=======
-import requests
-from bs4 import BeautifulSoup
-
-
-#shampooraussucher
-
-url = "https://product-search.services.dmtech.com/de/search/static?allCategories.id=110101&pageSize=15&sort=editorial_relevance&currentPage=3&type=search-static"
-r = requests.get(url)
-
-
-j = r.json() 
-
-
-(len(j["products"]))
-i = 0
-while i< len(j["products"]):
-    artikelnummer = j["products"][i]["gtin"]
-    i = i+1
-
-
-
-
-#ingredientraussucher
-url = f"https://products.dm.de/product/de/products/gtins/{artikelnummer}?view=details"
-r = requests.get(url)
-
-
-l = r.json()
-j = l[0]
-
-ingredients = j["details"]["descriptionGroup"]["nonFoodIngredientsDescription"]["text"]
-
-ingredients_list = ingredients.split(" | ")
-print(ingredients_list)
->>>>>>> 8435d2e34c49719c04d26fcfc282a34b38d8b757
